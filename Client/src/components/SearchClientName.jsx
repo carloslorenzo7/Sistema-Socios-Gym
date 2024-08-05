6e0a58cfae50b05455996e91741c0a7f5817cc1f
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2024-08-05 19:19:22 -0300

    cambio en search client

--- Client/src/components/SearchClientName.jsx
+++ Client/src/components/SearchClientName.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const SearchClientName = ({ nombre }) => {
   const [client, setClient] = useState([]); // donde vamos a obtener datos recibidos de api
   const [error, setError] = useState(null);
-
+  const location=useLocation;
   useEffect(() => {
     if (!nombre) return;
 
@@ -20,6 +20,8 @@ const SearchClientName = ({ nombre }) => {
         );
 
         setClient(response.data);
+        console.log(setClient);
+        
         setError([]);
       } catch (error) {
         setError("Error al buscar clientes");
@@ -29,21 +31,31 @@ const SearchClientName = ({ nombre }) => {
     axiosClients();
   }, [nombre]);
 
+  useEffect(() => {
+    return () => {
+      setClient([]);
+      setError(null);
+    };
+  }, [location])
   return (
     <div className="max-w-4xl mx-auto mt-10 bg-white p-8 shadow-md rounded-lg">
       {error && <p>{error}</p>}
+      <ul className="mt-4">
+
       {client.length > 0
         ? client.map((c) => (
-            <div key={c.id}>
-              <Link to={`/dashboard/cliente/${c.id}`}>
-              <p>Nombre:{c.nombre}</p>
-              <p>Apellido{c.apellido}</p>
-              <p>Email:{c.email}</p>
-              <p>Dni:{c.dni}</p>
+            <li key={c.id}  className="pb-4 ">
+              <Link to={`/dashboard/cliente/${c.id}`} className="block space-y-2 border-b-2">
+              <p><strong>Nombre:</strong>{c.nombre}</p>
+              <p><strong>Apellido:</strong>{c.apellido}</p>
+              <p><strong>Email:</strong>{c.email}</p>
+              <p><strong>Dni:</strong>{c.dni}</p>
               </Link>
-            </div>
+            </li>
           ))
         : !error && <p>No se encontraron clientes</p>}
+
+      </ul>
     </div>
   );
 };
